990e5411bd8193c8edbfa8f13935428ff9695fd9
mdeantoni87@gmail.com <mdeantoni87@gmail.com@6b661105-8c5a-cc91-2134-fe70c0e07258>
2011-03-25 21:21:13 +0000

    sigo probando que funquen las cosas, y trate de meter el proyecto en el trunk

--- GoGame/src/main.java
+++ GoGame/src/main.java
@@ -5,7 +5,8 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo, probando svn + eclipse + commit xD");
+		System.out.println("Hola Mundo, probando svn + eclipse + commit " +
+				"+ otras cosas que estuve probando xD xD");
 
 	}
 

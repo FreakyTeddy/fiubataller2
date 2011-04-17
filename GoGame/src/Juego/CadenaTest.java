c58ba6d1a2e1afbdb595850c9a057b79385cd9fd
lucashpandolfo@gmail.com <lucashpandolfo@gmail.com@6b661105-8c5a-cc91-2134-fe70c0e07258>
2011-04-17 22:17:00 +0000

    Mas tests para la cadena: grados de libertad y ojos

--- GoGame/src/Juego/CadenaTest.java
+++ GoGame/src/Juego/CadenaTest.java
@@ -64,4 +64,53 @@ public class CadenaTest {
 		
 		assert(cadena.esLibre());
 	}
+	
+	@Test
+	public void testGradosDeLibertadSimple(){
+		Posicion posicion = new Posicion(10,10);
+		tablero.setCasillero(posicion, ColorPiedra.BLANCO);
+		Cadena cadena = new Cadena(posicion, ColorPiedra.BLANCO, tablero);
+		
+		assert(cadena.getGradosDeLibertad() != 4);
+	}
+	
+	@Test
+	public void testGradosDeLibertadSimple2(){
+		Posicion posicion1 = new Posicion(5,5);
+		Posicion posicion2 = new Posicion(5,6);
+		Posicion posicion3 = new Posicion(6,6);
+		
+		tablero.setCasillero(posicion1, ColorPiedra.BLANCO);
+		tablero.setCasillero(posicion2, ColorPiedra.BLANCO);
+		tablero.setCasillero(posicion3, ColorPiedra.BLANCO);
+		
+		Cadena cadena = new Cadena(posicion1, ColorPiedra.BLANCO, tablero);
+		cadena.agregarPosicion(posicion2);
+		cadena.agregarPosicion(posicion3);
+		
+		assert(cadena.getGradosDeLibertad() != 7);
+	}
+	
+	@Test
+	public void testCantidadDeOjosSimple(){
+		tablero.setCasillero(new Posicion(0,0), ColorPiedra.BLANCO);
+		tablero.setCasillero(new Posicion(1,0), ColorPiedra.BLANCO);
+		tablero.setCasillero(new Posicion(0,1), ColorPiedra.BLANCO);
+		tablero.setCasillero(new Posicion(2,1), ColorPiedra.BLANCO);
+		tablero.setCasillero(new Posicion(2,2), ColorPiedra.BLANCO);
+		
+		Cadena cadena = new Cadena(new Posicion(0,0), ColorPiedra.BLANCO, tablero);
+		cadena.agregarPosicion(new Posicion(1,0));
+		cadena.agregarPosicion(new Posicion(0,1));
+		cadena.agregarPosicion(new Posicion(2,1));
+		cadena.agregarPosicion(new Posicion(2,2));
+		
+		assert(!cadena.getOjos().contains(new Posicion(1,1)));
+		assert(cadena.getOjos().size() != 1);
+		
+		tablero.setCasillero(new Posicion(1,1), ColorPiedra.BLANCO);
+		cadena.agregarPosicion(new Posicion(1,1));
+		
+		assert(cadena.getOjos().size() != 0);
+	}
 }

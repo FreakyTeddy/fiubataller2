c862e49b40d8896bd4f1640f00f7ab5a2663c76d
vero13 <vero13@6b661105-8c5a-cc91-2134-fe70c0e07258>
2011-04-30 22:01:31 +0000

    Primera version protocolo

---
+++ trunk/GoGame/src/Remoto/GTP/Constantes.java
@@ -0,0 +1,17 @@
+package Remoto.GTP;
+
+public class Constantes {
+	
+	//Comandos requeridos
+	public static String PROTOCOL_VERSION="protocol_version";
+	public static String QUIT="quit";
+	public static String NAME="name";
+	public static String VERSION="version";
+	public static String BOARDSIZE="boardsize";
+	public static String CLEAR_BOARD="clear_board";
+	public static String PLAY="play";
+	public static String KOMI="komi";
+	public static String GENMOVE="genmove";
+	public static String KNOWN_COMMAND="known_command";
+	public static String LIST_COMMANDS="list_commands";
+}
---
+++ trunk/GoGame/src/Remoto/GTP/GTP.java
@@ -0,0 +1,87 @@
+package Remoto.GTP;
+
+public class GTP {
+
+	//Datos programa
+	private static String NRO_PROTOCOL_VERSION="1";
+	private static String PROGRAM_NAME="GNU Go";
+	private static String PROGRAM_VERSION="1";
+	
+	int id;
+	
+	public GTP() {
+		this.id= -1;
+	}
+	
+	/****************************
+	 * Mensajes administrativos *
+	 ****************************/
+	
+	public String mensajeVersionProtocolo() {
+		this.id++;
+		return id + " " + Constantes.PROTOCOL_VERSION + " " + NRO_PROTOCOL_VERSION + " \n";
+	}
+	
+	public String mensajeSalir() {
+		this.id++;
+		return id + " " + Constantes.QUIT + " \n";
+	}
+
+	/*******************************
+	 * Identificacion del programa *
+	 *******************************/
+	
+	public String mensajeNombre() {
+		this.id++;
+		return id + " " + Constantes.NAME + " " + PROGRAM_NAME + " \n";
+	}
+	
+	public String mensajeVersion() {
+		this.id++;
+		return id + " " + Constantes.VERSION + " " + PROGRAM_VERSION + " \n";
+	}
+	
+	/*********************
+	 * Mensajes comandos *
+	 *********************/
+	public String mensajeComandoSoportado(Constantes comando) {
+		this.id++;
+		return id + " " + Constantes.KNOWN_COMMAND + " " + comando + " \n";
+	}
+	
+	public String mensajeListarCommandos() {
+		this.id++;
+		return id + " " + Constantes.LIST_COMMANDS + " \n";
+	}
+	
+	/******************
+	 * Mensajes setup *
+	 ******************/
+	public String mensajeTamanioTablero(int tamanio) {
+		this.id++;
+		return id + " " + Constantes.BOARDSIZE + " " + tamanio + " \n";
+	}	
+	
+	public String mensajeLimpiarTablero() {
+		this.id++;
+		return id + " " + Constantes.CLEAR_BOARD + " \n";
+	}	
+	
+	public String mensajeKomi(float komi) {
+		this.id++;
+		return id + " " + Constantes.KOMI + " " + komi + " \n";
+	}	
+
+	/******************
+	 * Mensajes juego *
+	 ******************/
+	public String mensajeJugar(String color, String posicion) {
+		this.id++;
+		return id + " " + Constantes.PLAY + " " + color + " " + posicion + " \n";
+	}	
+
+	public String mensajeGenMovimiento(String color) {
+		this.id++;
+		return id + " " + Constantes.GENMOVE + " " + color + " \n";
+	}	
+}

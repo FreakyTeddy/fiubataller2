59dad6357d4683baaa6605b4724ad4c905dcea52
mdeantoni87@gmail.com <mdeantoni87@gmail.com@6b661105-8c5a-cc91-2134-fe70c0e07258>
2011-05-29 18:54:23 +0000

    Re agrego lo que tenia de sonidos. pero no se porque el muy choto ahora no reproduce! es lo mismo que tenia antes!!!!

--- trunk/GoGame/src/vista/PathImages.java
+++ trunk/GoGame/src/vista/PathImages.java
@@ -8,4 +8,6 @@ class PathImages {
 	public static final String Blanca = "./images/blanca.png";
 	public static final String Negra = "./images/negra.png";
 	public static final String MenuInicio = "./images/go-game2.jpg";
+	public static final String SonidoPieza1 = "./sounds/pieza1.wav";
+	public static final String SonidoPieza2 = "./sounds/pieza2.wav";
 }
